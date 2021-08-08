881ad15b8d551ba203c3cde459e9a33054a53fc1
Steven Montoya <steve5mv5@gmail.com>
2021-08-08 23:34:20 +0200

    playeer stadistics and some changes in streamlit

--- client/dashboard.py
+++ client/dashboard.py
@@ -16,6 +16,10 @@ def draw_bar(x_data, y_data, title):
 
     fig.layout.plot_bgcolor = '#141414'
     fig.layout.title = title
+    fig.update_layout(
+        autosize=False,
+        width=1200,
+        height=800,)
 
     st.plotly_chart(fig, config={
         'displayModeBar': False
@@ -43,6 +47,9 @@ def draw_dash_board():
     draw_conditional_button(goal_button, 'Goals',
                             'goal_scored', teams, 'Total goals', get_data_from_stadistics)
 
+    draw_conditional_button(goal_conceded_button, 'Goals conceded',
+                            'goal_conceded', teams, 'Total goals', get_data_from_stadistics)
+
     draw_conditional_button(possession_button, 'Mean possession',
                             'mean_possession', teams, 'Possession', get_data_from_stadistics)
 
@@ -52,16 +59,53 @@ def draw_dash_board():
     draw_conditional_button(cards_button, 'Total cards',
                             'yellow_cards', teams, 'Total cards', get_data_from_stadistics)
 
+    draw_conditional_button(extra_time_button, 'Extra times',
+                            'extra_times', teams, 'Extra times', get_data_from_stadistics)
+
+
+def draw_teams_page():
+    st.write('')
+
 
 st.write("# EURO CUP 2020")
 response = requests.get('http://localhost:8000/stadistics')
 team_stadistics = response.json()
 
-st.markdown(""" <style> div.stButton > button:first-child {
-  background-color: rgb(23, 180, 207);
-} </style>""", unsafe_allow_html=True)
+st.markdown(""" 
+<style> 
+    div.stButton > button:first-child {
+        background-color: rgb(23, 180, 207);
+    }
+</style>
+     """, unsafe_allow_html=True)
 
+st.markdown(
+    f"""
+<style>
+    .reportview-container .main .block-container{{
+        max-width: 80%;
+   
+    }}
 
-goal_button, possession_button, shots_button, cards_button = st.beta_columns(4)
+    #MainMenu {{
+        visibility: hidden;
+    }}
+    
+    footer {{
+        visibility: hidden;
+    }}
 
-draw_dash_board()
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+goal_button, goal_conceded_button, possession_button, shots_button, cards_button, extra_time_button = st.beta_columns(
+    6)
+
+page = st.sidebar.selectbox("Select page", ["Dashboard", "Teams"])
+
+if page == 'Dashboard':
+    draw_dash_board()
+if page == 'Teams':
+    draw_teams_page()
