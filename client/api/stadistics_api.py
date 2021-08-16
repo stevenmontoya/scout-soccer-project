e4ea2d7b6d6a15844085d23ddf1b81040ed8ef6b
Steven Montoya <steve5mv5@gmail.com>
2021-08-16 20:15:16 +0200

    code refactor

--- client/api/stadistics_api.py
+++ client/api/stadistics_api.py
@@ -1,13 +1,13 @@
 from requests.models import Response
 import api.instance as api_instance
-import api.utils.response as parse
+import api.utils.mapper as mapper
 
 urlBase = 'http://localhost:8000/stadistics'
 
 
 def get_all(key=None):
     team_stadistics = api_instance.get(urlBase)
-    return parse.list_response(team_stadistics, key)
+    return mapper.response_mapper(team_stadistics, key)
 
 
 def get_stadistics_team(team):
--- client/api/utils/mapper.py
+++ client/api/utils/mapper.py
@@ -3,3 +3,9 @@ import pandas as pd
 
 def json_to_dataframe(json):
     return pd.DataFrame(json)
+
+
+def response_mapper(response_list, key=None):
+    if key:
+        return [stadistic[key] for stadistic in response_list]
+    return response_list
